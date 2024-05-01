bbb005a88450a127e4ee1f169d344bc10d69e0d5
Jacob Amezquita <113970708+Hacube@users.noreply.github.com>
2024-05-01 10:03:28 -0700

    Update WMM.m

--- Functions/WMM.m
+++ Functions/WMM.m
@@ -1,4 +1,4 @@
-function Tb_b = WMM(m_b, r_ECI, q_b_ECI, JD_0, t)
+function Tb_b = WMM(r_ECI, q_b_ECI, JD_0, t)
 % Function of World Magn. Model
 
 % DGRF 2020 (given)
@@ -7,6 +7,7 @@ g_11 = -1450.9;  %nT
 h_11 = 4652.5;   %nT
 g_10 = -29404.8; %nT
 m_ECEF = a^3*[g_11; h_11; g_10];
+m_b = [0;0;-0.5];
 
 I = eye(3);    % Identity
 
@@ -17,7 +18,6 @@ eps = [eps1; eps2; eps3];
 
 eda = q_b_ECI(4);
 
-% Solve
 eps_cross = [0,  -eps3, eps2;
              eps3,  0, -eps1;
              -eps2, eps1, 0];
@@ -27,8 +27,6 @@ C_b_ECI = (2*eda^2 - 1)*I + 2*(eps*eps') - 2*eda*eps_cross; % rotation w/ quat
 r_ECEF = eci2ecef_manual(JD_0, r_ECI);
 C_ECI_ECEF = r_ECI*r_ECEF';
 
-m_b = C_b_ECI*(C_ECI_ECEF*m_ECEF)
-
 % Solve
 B_ECEF = (3*(m_ECEF'*r_ECEF)*r_ECEF - norm(r_ECEF)^2*m_ECEF)/(norm(r_ECEF))^5; % Mag. field vect in ECEF
 
@@ -36,7 +34,7 @@ B_ECI = C_ECI_ECEF*B_ECEF;
 
 B_b = C_b_ECI*B_ECI;
 
-Tb_b = m_b*B_b;
+Tb_b = cross(m_b,B_b);
 
 end
 
